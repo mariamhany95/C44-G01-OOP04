8d8a35c3a42e3b886d22caeaa47e58f1762b348d
MariamHany <mariamhanyzarief55@gmail.com>
2025-08-03 22:53:34 +0300

    Video-4

---
+++ demo/OverLoading/Complex.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo.OverLoading
+{
+    internal class Complexx
+    {
+        #region Properties
+
+        public int Real { get; set; }
+        public int Img { get; set; }
+        #endregion
+
+        #region Methods
+        public override string ToString()
+        {
+            return $"{Real} + {Img}i ";
+        }
+        #endregion
+
+        #region Operator OverLoading
+
+        public static Complexx operator +(Complexx left, Complexx right)
+        {
+            return new Complexx()
+            {
+                Real = (left?.Real ?? 0) + (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) + (right?.Img??0),
+            };
+        }
+
+
+        public static Complexx operator -(Complexx left, Complexx right)
+        {
+            return new Complexx() 
+            {
+                Real = (left?.Real ?? 0) - (right?.Real ?? 0),
+                Img = (left?.Img ?? 0) - ( right?.Img ?? 0)
+            };
+
+
+
+        }
+
+            #endregion
+        }
+    }
+
+
--- demo/Program.cs
+++ demo/Program.cs
@@ -1,52 +1,75 @@
-﻿namespace demo
+﻿using System.Numerics;
+
+
+using demo.OverLoading;
+namespace demo
 {
     internal class Program
     {
 
         #region Two numbers method
-        static int SumNumbers(int x, int y)
-        {
-            return x + y;
-        }
-
-        static double SumNumbers(double x, double y)
-        {
-            return x + y;
-        }
-
-        static int SumNumbers(int x, int y, int z)
-        {
-            return x + y + z;
-        }
-
-        ////هنا الكلام ده invalid 
-        //static double SumTowNumbers(int a, int b)
-        //{
-        //    return (double)a + (double)b;
-        //}
-
-        static double SumNumbers(int x, double y)
-        {
-            return x + y;
-        }
-        //static double SumNumbers(double x, int y)
+        //static int SumNumbers(int x, int y)
         //{
         //    return x + y;
         //}
-        //في ال 2 methods دول ال combiler مش هيعمل error و لكن مش اصح حلجه علشان انا ممكن اعمل الكلام ده بال method by name 
+
+        //static double SumNumbers(double x, double y)
+        //{
+        //    return x + y;
+        //}
+
+        //static int SumNumbers(int x, int y, int z)
+        //{
+        //    return x + y + z;
+        //}
+
+        //////هنا الكلام ده invalid 
+        ////static double SumTowNumbers(int a, int b)
+        ////{
+        ////    return (double)a + (double)b;
+        ////}
+
+        //static double SumNumbers(int x, double y)
+        //{
+        //    return x + y;
+        //}
+        ////static double SumNumbers(double x, int y)
+        ////{
+        ////    return x + y;
+        ////}
+        ////في ال 2 methods دول ال combiler مش هيعمل error و لكن مش اصح حلجه علشان انا ممكن اعمل الكلام ده بال method by name 
 
 
         #endregion
         static void Main(string[] args)
         {
-            #region Video-2
-            //Method Overloading
-            int a = 10, b = 20, c = 30;
-            int result = SumNumbers(a, b);
-            result = SumNumbers(a, b, c);
-            SumNumbers(10.10, 20.20);
-            SumNumbers(y: 1.2, x: 2.2);//ده كده method by name
-            Console.WriteLine(result); 
+            #region Video-3
+            ////Method Overloading
+            //int a = 10, b = 20, c = 30;
+            //int result = SumNumbers(a, b);
+            //result = SumNumbers(a, b, c);
+            //SumNumbers(10.10, 20.20);
+            //SumNumbers(y: 1.2, x: 2.2);//ده كده method by name
+            //Console.WriteLine(result);
+            #endregion
+
+            #region Video-4
+            //Operators Overloading
+
+            Complexx C1 = new Complexx();
+            C1.Real = 10;
+            C1.Img = 5;
+            Console.WriteLine(C1);
+
+            Complexx C2 = new Complexx() { Real=6 , Img=2};
+            Console.WriteLine(C2);
+
+            Complexx C3 = new Complexx();
+            C3 = C1 + C2;//Invalid before Operator overloading
+            C3 += C1;//valid because it a (C3=C3+C1) 
+            C3 = C1 - C2;//VALID AFTER OPERATOR OVERLOADING
+            Console.WriteLine($"C3={C3}");
+
             #endregion
         }
     }
